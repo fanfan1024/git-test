6fe5dfc165ff5845ed739721d057fa28e2d0b149
Fanny <Fanny@123.com>
2021-05-29 22:44:01 +0800

    first commit

---
+++ src/main/java/com/cn/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.cn.git;
+
+/**
+ * @author fanny
+ * @date 2021 - 22:37
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git"); //.sout
+    }
+}
